6246db6ef6fcfb6c967d516dc06bf000b3e6a10a
danoux10 <danybarbe10@gmail.com>
2023-07-13 12:29:09 +0200

    simplify calcul

--- js/calcule.js
+++ js/calcule.js
@@ -5,79 +5,31 @@ function calcule(){
   let input_point = document.getElementById('gold-point');
 
   // let pt = 0;
-  let rank_point=0;
-  let point= 0;
-
   const golden_gun = 3000;
-
   const win_point = 25;
-  const draw_point = 5;
 
-  const bronze_point = 300;
-  const silver_point = 450;
-  const gold_point = 600;
-  const platinum_point = 800;
-  const diamond_point = 1000;
-  const master_point = 1200;
-  const grand_master_point = 1500;
-
-  if(isNaN(value_point)) {
-    point = 0;
-  }
-  if(!isNaN(value_point)){
-    point = value_point;
+  const rank_points ={
+    'unselect' : 0,
+    'bronze' : 300,
+    'silver' : 450,
+    'gold' : 600,
+    'platinum' : 800,
+    'diamond' : 1000,
+    'master' : 1200,
+    'grand-master' : 1500,
   }
 
-  if(radioSelect.value == 'unselect'){
-    rank_point = 0;
-  }
-
-  if(radioSelect.value == 'bronze'){
-    rank_point = bronze_point;
-  }
-
-  if(radioSelect.value == 'silver'){
-    rank_point = silver_point;
-  }
-
-  if(radioSelect.value == 'gold'){
-    rank_point = gold_point;
-  }
-
-  if(radioSelect.value == 'platinum'){
-    rank_point = platinum_point;
-  }
-
-  if(radioSelect.value == 'diamond'){
-    rank_point = diamond_point;
-  }
-
-  if(radioSelect.value == 'master'){
-    rank_point = master_point;
-  }
-
-  if(radioSelect.value == 'grand-master'){
-    rank_point = grand_master_point;
-  }
+  let point = isNan(value_point) ? 0 : value_point;
+  let rank_point = rank_points[radioSelect.value] || 0 ;
 
   winView.innerHTML = '';
 
-  var first = document.createElement("span");
-  first.className = "first";
-  first.innerHTML = "•";
-
-  var second = document.createElement("span");
-  second.className = "second";
-  second.innerHTML = "•";
-
-  var third = document.createElement("span");
-  third.className = "third";
-  third.innerHTML = "•";
-
-  // Ajout des balises span à l'élément body
-  winView.appendChild(first);
-  winView.appendChild(second);
-  winView.appendChild(third);
+  for (let i = 0; i<3; i++){
+    const span = document.createElement('span');
+    span.className = (i === 0) ? 'first' : (i === 1) ? 'second' : 'third';
+    span.innerHTML = '•';
+    winView.appendChild(span);
+  }
 
   let point_total = point + rank_point;
   let restant = golden_gun - point_total;
--- js/loader.js
+++ js/loader.js
@@ -1,4 +1,10 @@
 const loader = document.getElementById('loader');
+function load(){
+  loader.classList.add('');
+  loader.classList.remove('');
+  loader.classList.toggle('');
+  loader.classList.contains('');
+}
 window.addEventListener('load',function(){
   loader.classList.add('hidden');
 });
